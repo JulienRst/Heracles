cc294d9d1b4efb4d122e02700c881a560339df37
Julien Rousset <julien.r@geronimostudios.com>
2019-11-02 16:04:53 +0100

    Added Three + basic scss setup

---
+++ src/views/Root.vue
@@ -0,0 +1,23 @@
+<template>
+  <div ref="root" id="root">
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import * as THREE from 'three';
+
+@Component({
+  name: 'Root'
+})
+export default class Root extends Vue {
+  private scene: THREE.Scene = new THREE.Scene();
+  private camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  private renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer();
+
+  public mounted () {
+    const root = this.$refs['root'];
+    root.append(this.renderer.domElement);
+  }
+}
+</script>
